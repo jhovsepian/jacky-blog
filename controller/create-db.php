4ca87b9ca9c4ae148bdfefb781cce2f468a8920b
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-10-28 08:49:00 -0700

    almost done with video 12

---
+++ controller/create-db.php
@@ -0,0 +1,23 @@
+<?php
+// The directory of the file. If used inside an include, the directory of the included file is returned
+require_once(__DIR__ . "/../model/database.php");
+
+$connection = new mysqli($host, $username, $password);
+
+if($connection->connect_error) {
+     die("Error: " . $connection->connect_error);
+}
+
+$exists = $connection->select_db($database);
+// code below lets you know the database doesnt exist
+if(!$exists) {
+	$query = $connection->query("CREATE DATABASE $database");
+    // if exists it will say successfully created.
+	if($query) {
+        echo "Successfully created database; " . $database;
+	}
+}
+
+$connection->close();
+
+?>
