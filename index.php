4b795cc1d3b9de9706cecc111b0e2c931e115440
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-11-04 08:55:25 -0800

    video 17,18

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // made new views
 // added header and footer to blog
 	require_once (__DIR__ . "/view/header.php");
+	require_once (__DIR__ . "/view/navigation.php");
 	require_once (__DIR__ . "/controller/create-db.php");
 	require_once (__DIR__ . "/view/footer.php");
 ?>
