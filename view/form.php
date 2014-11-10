90e82c25c92384f48b750917cab929e1fa7e11af
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-11-10 10:28:29 -0800

    video 22

--- view/form.php
+++ view/form.php
@@ -1,7 +1,13 @@
+<?php
+	require_once (__DIR__ . "/../model/config.php");
+?>
+
 <!-- This created a blog post that i can submit into my phpmyadmin. -->
 <!-- I can write whatever i want and submit it -->
 <h1>Create Blog Post</h1>
-<form>
+<!-- to echo out the title and the post -->
+<!-- this is to submit whatever i wrote into my controller/ create post -->
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
 	<div>
 		<label for="title">Title: </label>
 		<input type="text" name="title" />
