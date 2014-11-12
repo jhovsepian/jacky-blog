fa245cc3a5f57747f80db6ead7418ee2d3f989e6
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-11-12 08:42:21 -0800

    video 23

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,8 @@
 <?php
+	require_once (__DIR__ . "/../model/database.php")
+// to create a new mysqli object
+	// creating a connection to the database
+	$connection = new mysqli($host, $username, $password, $database); 
 // this sends info to the title in create-db
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 // this sends info to the post	in create-db
@@ -7,3 +11,5 @@
 	// this is to echo out my title and my post 
 	echo "<p>Title: $title</p>"; 
 	echo "<p>Post: $post</p>";
+// to close the connection to the database
+	$connection->close();
