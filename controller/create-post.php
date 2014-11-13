36d3fab9de0f2271cec89ef06d0cdeac562c190f
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-11-13 08:50:43 -0800

    video 24

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,15 +1,24 @@
 <?php
-	require_once (__DIR__ . "/../model/database.php")
+	require_once (__DIR__ . "/../model/database.php");
 // to create a new mysqli object
 	// creating a connection to the database
+	//this is to send the information to the create post file
 	$connection = new mysqli($host, $username, $password, $database); 
 // this sends info to the title in create-db
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 // this sends info to the post	in create-db
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-	
-	// this is to echo out my title and my post 
-	echo "<p>Title: $title</p>"; 
-	echo "<p>Post: $post</p>";
-// to close the connection to the database
+// created a query
+	// this created a query to insert information
+	// we want to insert things to the posts table
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+// if it successfully inserted info to the post table this is what it will say.. 
+	if($query) {
+		echo "<p>Successfully inserted post: $title</p>";
+	}
+	// if it didnt successfully insert info to the post table it will have an error
+	else {
+		echo "<p>$connection->error</p>";
+	}
+
 	$connection->close();
--- view/header.php
+++ view/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 <html>
 <head>
   <link rel="stylesheet" type="text/css" href="css/main.css">
