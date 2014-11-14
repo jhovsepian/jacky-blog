16044b75ce0a13700f84279f2c3abb64c998cf03
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-11-14 08:49:27 -0800

    made a new database class; video 26

---
+++ model/Database.php
@@ -0,0 +1,10 @@
+<?php
+class Database {
+	// this code only is accessed in this class because its private
+	// I made a new Database class
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database;
+}
