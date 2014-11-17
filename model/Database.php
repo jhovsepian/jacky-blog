91a3ed9e1af6e56e2a69947e643c111f32f5666c
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-11-17 10:20:16 -0800

    video 27 with comments, we created a constructer

--- model/Database.php
+++ model/Database.php
@@ -1,6 +1,6 @@
 <?php
 // THIS IS A NEW DATABASE.
-// the database object is created by the class
+// the database object is created by the class   
 class Database {
 	// this code only is accessed in this class because its private. others are public
 	// we dont want other files accessing this information within the database class
@@ -9,4 +9,13 @@ class Database {
 	private $username;
 	private $password;
 	private $database;
+// create a constructer. 
+// a constructer is important because it allows us to bulid objects of type database.
+// when we construct our object, we are going to pass the information to the host, username, password, database.
+	public function __construct($host, $username, $password, $database) {
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	}
 }
