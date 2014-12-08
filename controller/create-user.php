cfc9b6adebee62afc2f514a0af14c45051d0c958
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-12-08 10:37:33 -0800

    video 43 we made a salt for our password so nobody see the password.

--- controller/create-user.php
+++ controller/create-user.php
@@ -4,5 +4,9 @@
 	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
-// this is to echo out my email when i click submit.
-	echo $email . " - " . $username . " - " . $password;
+// to show the password
+	echo $password;
+// this is too give random numbers for the password so no one knows it.
+	$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+// this is to salt the password
+	echo $salt;
