644a97145bad21990e15e6934c5985b6edb5988e
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-12-08 10:27:38 -0800

    video 42 we created the user and stored  the user with the new database table.

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,3 +1,8 @@
 <?php
-// this echos out what it says
-	echo "Successfully created user";
+	require_once (__DIR__ . "/../model/config.php");
+// create the user and store the user with the new database table.
+	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+// this is to echo out my email when i click submit.
+	echo $email . " - " . $username . " - " . $password;
