b07ecde4b2e846415f384c42f2ac5546b03a2b2f
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-12-11 09:01:50 -0800

    video 47 we are picking the user information from the database

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,3 +1,8 @@
 <?php
-// to echo out this:
-echo "Successfully logged in user.";
+	require_once (__DIR__ . "/../model/config.php");
+
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+// someone picks a new username then the query makes the password.
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+?>
