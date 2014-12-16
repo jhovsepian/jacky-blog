4a0e7b321be31c0646eda22305c37fa14c2169d5
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-12-16 08:05:27 -0800

    read more

--- view/footer.php
+++ view/footer.php
@@ -1,2 +1,5 @@
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="js/readmore.min.js"></script>
+	<script src="js/main.js"></script>
 	</body>
 </html>
