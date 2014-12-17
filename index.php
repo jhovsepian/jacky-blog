4f4bc84941141d0122b357a8cddeccf82e0eb4b1
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2014-12-17 07:59:58 -0800

    fixed up some of my code.  from yesterday

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 	require_once (__DIR__ . "/view/navigation.php");
 	}
 	require_once (__DIR__ . "/controller/create-db.php");
-	require_once (__DIR__ . "/view/footer.php");
+	
 	require_once (__DIR__ . "/controller/read-posts.php");
+	require_once (__DIR__ . "/view/footer.php");
 ?>
 
